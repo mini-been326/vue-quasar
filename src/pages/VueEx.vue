9f6564d3f2c80688cd4e75b40c7c7732bf23ba88
mini-been326 <68268304+mini-been326@users.noreply.github.com>
2023-04-22 17:53:19 +0900

    3week commit

---
+++ src/pages/VueEx.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name:' ',
+  components:{},
+  data(){
+    return{
+
+    }
+  },
+  computed:{
+
+  },
+  watch:{
+
+  },
+  mounted:{
+
+  },
+  method:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
