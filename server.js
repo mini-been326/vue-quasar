7b52e245b4f9b40ae5d01927d123fd429574bb31
mini-been326 <68268304+mini-been326@users.noreply.github.com>
2023-04-23 19:41:52 +0900

    6week commit

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ let corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true }));
+
 //const todoRouter = require('./api/todo')
 //app.use('/api',router);
 
